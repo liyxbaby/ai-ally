ac4d8500daff81ac5cde2935c993dc0ca97dc6f8
liyxbaby <liyxbaby@proton.me>
2023-04-26 18:52:03 +0000

    Fix name

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,4 +5,17 @@ use database::{Database, Message, NewMessage, CompanionView, UserView, ConfigMod
 mod long_term_mem;
 use long_term_mem::LongTermMem;
 mod dialogue_tuning;
-use dialogue_
+use dialogue_tuning::DialogueTuning;
+mod character_card;
+use character_card::CharacterCard;
+use serde::Deserialize;
+mod llm;
+use crate::llm::prompt;
+
+use std::fs;
+use std::fs::File;
+use std::io::{Write, Read};
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body(include_str!("../../dist/index.
