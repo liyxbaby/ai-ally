9765d83197ec4e4894d75623bd6a315478dcfcce
liyxbaby <liyxbaby@proton.me>
2023-04-26 18:27:11 +0000

    Add travis status

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,1 +1,8 @@
-use actix_web::{get, 
+use actix_web::{get, post, delete, put, App, web, HttpResponse, HttpServer};
+use futures_util::StreamExt as _;
+mod database;
+use database::{Database, Message, NewMessage, CompanionView, UserView, ConfigModify};
+mod long_term_mem;
+use long_term_mem::LongTermMem;
+mod dialogue_tuning;
+use dialogue_
