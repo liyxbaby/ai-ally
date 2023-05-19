cd9fbe5795daee363f047cd828372c89ebe75f7d
liyxbaby <liyxbaby@proton.me>
2023-05-19 08:25:34 +0000

    comm

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,10 @@ import './App.scss'
 
 import { ThemeProvider } from "@/components/theme-provider"
 import Footer from './components/Footer'
-import ChatWindow from './compo
+import ChatWindow from './components/ChatWindow'
+import { MessagesProvider } from './components/context/messageContext'
+import { UserDataProvider } from './components/context/userContext'
+import { CompanionDataProvider } from './components/context/companionContext'
+import { ConfigProvider } from './components/context/configContext'
+
+import { Toaster
