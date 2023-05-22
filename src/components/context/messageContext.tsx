5aed7dc9789c320e2a14fa3ba206bcb87014c6ee
liyxbaby <liyxbaby@proton.me>
2023-05-22 16:39:52 +0000

    Update the README

--- src/components/context/messageContext.tsx
+++ src/components/context/messageContext.tsx
@@ -14,4 +14,14 @@ interface MessagesContextType {
   resetStart : () => void;
 }
 
-const MessagesContext = createContext<Message
+const MessagesContext = createContext<MessagesContextType | undefined>(undefined);
+
+export const useMessages = () => {
+  const context = useContext(MessagesContext);
+  if (!context) {
+    throw new Error('useMessages must be used within a MessagesProvider');
+  }
+  return context;
+};
+
+export const MessagesProvider: React.FC<MessagesProviderProps
