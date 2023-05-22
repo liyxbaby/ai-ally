a942dddaa37ab5aa9a0c7e839060f045677c66da
liyxbaby <liyxbaby@proton.me>
2023-05-22 13:13:25 +0000

    finish refactoring

--- src/components/context/messageContext.tsx
+++ src/components/context/messageContext.tsx
@@ -2,4 +2,16 @@ import React, { createContext, useState, useContext, useEffect, ReactNode } from
 import { MessageInterface } from '../interfaces/Message';
 import { toast } from "sonner";
 
-interface Me
+interface MessagesProviderProps {
+  children: ReactNode;
+}
+
+interface MessagesContextType {
+  messages: MessageInterface[];
+  refreshMessages: () => void;
+  pushMessage: (message: MessageInterface) => void;
+  loadMoreMessages: () => void;
+  resetStart : () => void;
+}
+
+const MessagesContext = createContext<Message
