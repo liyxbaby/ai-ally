6dd8c4229d0f53f05162c7d28624a458de9a4f4e
liyxbaby <liyxbaby@proton.me>
2023-05-23 18:05:12 +0000

    drm/radeon: fix bank tiling parameters on cayman

--- src/components/context/userContext.tsx
+++ src/components/context/userContext.tsx
@@ -1,2 +1,16 @@
 import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
 import { UserData } from '../interfaces/UserData';
+import { toast } from "sonner";
+
+interface UserDataProviderProps {
+  children: ReactNode;
+}
+
+interface UserDataContextType {
+  userData: UserData | null;
+  refreshUserData: () => void;
+}
+
+const UserDataContext = createContext<UserDataContextType | null>(null);
+
+export const UserDataProvider: React.FC<UserDa
