aabe91035d027b07578e6ebf1ea2755901a31bc3
liyxbaby <liyxbaby@proton.me>
2023-05-25 20:42:00 +0000

    rounded corners

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -15,4 +15,20 @@ import { Textarea } from "../ui/textarea"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Info } from "lucide-react"
 
-import companionAvatar from ".
+import companionAvatar from "../../assets/companion_avatar.jpg";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { updateCompanionData,  useCompanionData } from "../context/companionContext"
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
