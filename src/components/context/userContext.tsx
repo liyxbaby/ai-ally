219b2a91c790eec1df835647fcf53b74095ee89c
liyxbaby <liyxbaby@proton.me>
2023-05-25 06:21:20 +0000

    ping travis

--- src/components/context/userContext.tsx
+++ src/components/context/userContext.tsx
@@ -13,4 +13,14 @@ interface UserDataContextType {
 
 const UserDataContext = createContext<UserDataContextType | null>(null);
 
-export const UserDataProvider: React.FC<UserDa
+export const UserDataProvider: React.FC<UserDataProviderProps> = ({ children }) => {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [refreshData, setRefreshData] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchUserData().then((data) => {
+      setUserData(data);
+    });
+  }, [refreshData]);
+
+  const fetchUserData = as
