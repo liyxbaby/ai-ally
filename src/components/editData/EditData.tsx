dab712a4dc314b8923c17a023ceb663e06ca143f
liyxbaby <liyxbaby@proton.me>
2023-06-09 17:46:57 +0000

    Automated merge with file:///home/martin/work/jfx-80-sync/rt

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -398,4 +398,6 @@ export function EditData() {
             <div className="flex items-center space-x-2">
             <input
               type="checkbox"
-              className="peer h-4 w-4 shrink-0 rounded-sm border bor
+              className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              checked={companionFormData.roleplay}
+              onChange={(e: React.ChangeEven
