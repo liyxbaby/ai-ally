bc981aff19eb5dfc66863273c1a607b0881fb615
liyxbaby <liyxbaby@proton.me>
2023-06-12 22:00:41 +0000

    packageneric: scons

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -419,4 +419,7 @@ export function EditData() {
                 </div>
             </label>
           </div>
-          <div className="flex item
+          <div className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 da
