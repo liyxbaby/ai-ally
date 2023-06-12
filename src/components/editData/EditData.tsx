15a48c418faa8b3407c199e63f8004b438342698
liyxbaby <liyxbaby@proton.me>
2023-06-12 12:39:32 +0000

    Bump version to 0.0.8

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -400,4 +400,10 @@ export function EditData() {
               type="checkbox"
               className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
               checked={companionFormData.roleplay}
-              onChange={(e: React.ChangeEven
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCompanionFormData({ ...companionFormData, roleplay: e.target.checked })}
+            />
+            <label
+              htmlFor="roleplay"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 flex flex-row gap-2"
+            >
+             
