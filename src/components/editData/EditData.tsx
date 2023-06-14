0f32161a5f369f39f2da4693c703a5ca68887b34
liyxbaby <liyxbaby@proton.me>
2023-06-14 13:53:21 +0000

    Fix packaging

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -425,4 +425,11 @@ export function EditData() {
               className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
               id="dialogueTuning"
               checked={companionFormData.dialogue_tuning}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCompanionFormData({ ...companionFormData, dialogue_tuni
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCompanionFormData({ ...companionFormData, dialogue_tuning: e.target.checked })}
+            />
+            <label
+              htmlFor="dialogueTuning"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 flex flex-row gap-2"
+            >
+              <div className="flex items-center gap-2">
+                Dialo
