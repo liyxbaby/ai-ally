828a4131f47bd95647a7e53bc328014b65b85207
liyxbaby <liyxbaby@proton.me>
2023-06-14 10:10:45 +0000

    Updated FFmpeg

--- src/components/editData/EditData.tsx
+++ src/components/editData/EditData.tsx
@@ -422,4 +422,7 @@ export function EditData() {
           <div className="flex items-center space-x-2">
             <input
               type="checkbox"
-              className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 da
+              className="peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+              id="dialogueTuning"
+              checked={companionFormData.dialogue_tuning}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCompanionFormData({ ...companionFormData, dialogue_tuni
