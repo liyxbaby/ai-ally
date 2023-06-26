2386d16ad97026a161bd081a6fef68e207c00eba
liyxbaby <liyxbaby@proton.me>
2023-06-26 10:48:25 +0000

    * Updates German

--- src/components/message/MessageScroll.tsx
+++ src/components/message/MessageScroll.tsx
@@ -1,4 +1,13 @@
 import { useState, useEffect, useRef } from 'react';
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Message } from "./Message";
-import { useMessages } from "../context/mess
+import { useMessages } from "../context/messageContext";
+
+export function MessageScroll() {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const { messages, loadMoreMessages } = useMessages();
+  const [hasMoreMessages, setHasMoreMessages] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeigh
