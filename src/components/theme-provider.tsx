dc0b4fdd98b52938121846ba443db735f8226bdb
liyxbaby <liyxbaby@proton.me>
2023-06-27 21:16:51 +0000

    - rename

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,3 +1,21 @@
 import { createContext, useContext, useEffect, useState } from "react"
 
-type Theme = "dark
+type Theme = "dark" | "light" | "system"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  defaultTheme?: Theme
+  storageKey?: string
+}
+
+type ThemeProviderState = {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+}
+
+const initialState: ThemeProviderState = {
+  theme: "system",
+  setTheme: () => null,
+}
+
+const 
