24f5d4462d9ca661daf2c4442fc68e6347f4b9a3
liyxbaby <liyxbaby@proton.me>
2023-06-27 20:18:30 +0000

    Added commentary

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -25,4 +25,13 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThem
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
