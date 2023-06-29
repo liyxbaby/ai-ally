a2b37bb433df3804df0dd17140a66fb5a8e30f25
liyxbaby <liyxbaby@proton.me>
2023-06-29 20:26:55 +0000

    v2.8.1

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -30,4 +30,19 @@ export function ThemeProvider({
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
   )
 
-  useEffe
+  useEffect(() => {
+    const root = window.document.documentElement
+
+    root.classList.remove("light", "dark")
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light"
+
+      root.classList.add(systemTheme)
+      return
+    }
+
+    root.cla
