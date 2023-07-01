86fe8a4c3c2f9b5d6e8623b669404aff8cf26f01
liyxbaby <liyxbaby@proton.me>
2023-07-01 06:57:39 +0000

    aliases

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -59,4 +59,15 @@ export function ThemeProvider({
   return (
     <ThemeProviderContext.Provider {...props} value={value}>
       {children}
-    </ThemeProviderContext.Provid
+    </ThemeProviderContext.Provider>
+  )
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext)
+
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider")
+
+  return context
+}
