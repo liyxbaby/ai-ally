1609186d0083d34d1eb72a64bbb50d5131c1db9e
liyxbaby <liyxbaby@proton.me>
2023-07-01 07:28:46 +0000

    groom code

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,4 +1,13 @@
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
-impor
+import { cn } from "@/lib/utils"
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex h-10 w-10 shrink-0 overflow-hidden roun
