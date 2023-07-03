9c539ffd7c00db67e3f64da0f1d52fad230cbf8c
liyxbaby <liyxbaby@proton.me>
2023-07-03 11:59:46 +0000

    added missing function

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,4 +11,9 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-        des
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80"
