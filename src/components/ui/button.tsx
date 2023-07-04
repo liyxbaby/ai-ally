e5941359f64fd46684a9c25d5438e10d8d3188e6
liyxbaby <liyxbaby@proton.me>
2023-07-04 13:01:53 +0000

    gem version bump

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -41,4 +41,17 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, s
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
+export { Button, buttonVariants }
