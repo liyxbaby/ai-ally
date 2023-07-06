151ba63ef0f12f6db53545a720de12a7ceb425a4
liyxbaby <liyxbaby@proton.me>
2023-07-06 17:48:08 +0000

    Branch merge from 5.5

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -56,4 +56,15 @@ CardDescription.displayName = "CardDescription"
 const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ className
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+))
+CardContent.displayName = "CardContent"
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items
