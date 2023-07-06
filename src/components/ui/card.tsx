0f2f9656f9ed8e7f13af9144c60748f868c2d181
liyxbaby <liyxbaby@proton.me>
2023-07-06 16:34:09 +0000

    gpiolib: Don't return -EPROBE_DEFER to sysfs, or for invalid gpios

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -42,4 +42,18 @@ const CardTitle = React.forwardRef<
 CardTitle.displayName = "CardTitle"
 
 const CardDescription = React.forwardRef<
-  HTM
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+CardDescription.displayName = "CardDescription"
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className
