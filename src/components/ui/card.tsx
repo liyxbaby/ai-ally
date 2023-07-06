7265fd7a7b7d8a2d49dd582e29386474e5c1e89d
liyxbaby <liyxbaby@proton.me>
2023-07-06 09:00:42 +0000

    bump version to 0.6.0

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -15,4 +15,19 @@ const Card = React.forwardRef<
     {...props}
   />
 ))
-Card.displayName = "Car
+Card.displayName = "Card"
+
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+))
+CardHeader.displayName = "CardHeader"
+
+const CardTitle = React.forwardRef<
+  HTMLPara
