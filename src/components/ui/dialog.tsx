c31e745d3f949e9bc88b4889a9fb1d97f456c597
liyxbaby <liyxbaby@proton.me>
2023-07-07 10:58:52 +0000

    README.md -> README.rst

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,3 +1,16 @@
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { Cross2Ico
+import { Cross2Icon } from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+
+const Dialog = DialogPrimitive.Root
+
+const DialogTrigger = DialogPrimitive.Trigger
+
+const DialogPortal = DialogPrimitive.Portal
+
+const DialogClose = DialogPrimitive.Close
+
+const DialogOverlay = React.forwardRef<
+  React.ElementRef<typeo
