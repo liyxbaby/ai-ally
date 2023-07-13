9d7b5f6039569dc8ea7ba67d79922453700030db
liyxbaby <liyxbaby@proton.me>
2023-07-13 16:34:01 +0000

    x86/mce: Fix siginfo_t->si_addr value for non-recoverable memory faults

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -4,4 +4,14 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 import { cn } from "@/lib/utils"
 import { useLayoutEffect, useRef } from "react"
 
-const ScrollArea = React.forwardRef
+const ScrollArea = React.forwardRef<
+  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
+    inverted?: boolean
+  }
+>(({ className, children, inverted, ...props }, ref) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.
