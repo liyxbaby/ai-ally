00c5beee2306fd8ab3733a904724da2cd9d115da
liyxbaby <liyxbaby@proton.me>
2023-07-20 10:04:33 +0000

    Update start.js

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -8,4 +8,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
-  
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "grou
