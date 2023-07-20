e256ecd0f7aedb74f5fd9f5ed50538045925feaf
liyxbaby <liyxbaby@proton.me>
2023-07-20 16:13:49 +0000

    Added some fixes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,4 +14,16 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "grou
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
