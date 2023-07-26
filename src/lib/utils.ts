a77520f6e92a2dde9acba46e40e720427e9fcff4
liyxbaby <liyxbaby@proton.me>
2023-07-26 06:17:05 +0000

    iov_iter: hide iovec details behind ops function pointers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,2 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tai
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
